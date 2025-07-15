b14fcf3ec91a666331e7de9795e1dfd0bce11549
javzempic <javieralvarez2009@gmail.com>
2025-07-15 16:29:49 +0200

    cursoReact 15 de julio 16:29

---
+++ curso_React/Parte1/javascript.js
@@ -0,0 +1,11 @@
+
+//codigo JSX: es una extension para hacerlo mucho mas declarativa
+
+<React.Fragment>
+    <button data-id = "123">Button A</button>
+    <button data-id = "456">Button B</button>
+    <button data-id = "678">Button C</button>
+</React.Fragment>
+
+//para esto se encarga Babel o SVC que se encarga de traducir js
+
